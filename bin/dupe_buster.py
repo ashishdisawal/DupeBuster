f99785540f11f39e635f18cd0064ebbdbe5f537b
Ashish Disawal <ashish.disawal@evivehealth.com>
2017-03-27 15:18:15 +0530

    Basic script

---
+++ bin/dupe_buster.py
@@ -0,0 +1,12 @@
+import os
+import logging
+
+
+class DupeBuster(object):
+    def run(self):
+        logging.info("Running DupeBuster")
+
+
+if __name__ == '__main__':
+    dupe_buster = DupeBuster()
+    dupe_buster.run()
